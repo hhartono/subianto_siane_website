fa14826ef54874adeac70e40a8d10ad3ff61f418
Nugraha S. Fajar <nugie.nsf@gmail.com>
2015-07-06 09:27:22 +0700

    horizontal scrolling tidak aktif

--- application/views/public/contact.php
+++ application/views/public/contact.php
@@ -23,22 +23,31 @@
                                     </div> -->
 
                                     <div class="col-md-12">
-                                        <h4>Main Office</h4>
-                                        <ul>
-                                            <li><a href="https://www.google.co.id/maps/place/INERRE+Interior/@-6.900453,107.603322,17z/data=!3m1!4b1!4m2!3m1!1s0x2e68e64383f4eda3:0x420267de73972b84?hl=en" target="blank">Jl. Suka Mekar III No. 7, Bandung, Indonesia</a></li>
-                                            <li><a href="#">+62 22 2013973</a></li>
-                                            <li><a href="mailto:contact@subiantosiane.com">contact@subiantosiane.com</a></li>
-                                        </ul>
-                                        
-                                    </div>
-                                    <div class="col-md-12" style="padding-top:20px;">
-                                        <h4>Find Us On : </h4>
-                                        <ul>
-                                            <a href="https://www.facebook.com/inerre.interior">Facebook</a>|
-                                                <a href="https://twitter.com/inerre_interior">Twitter </a>|
-                                                <a href="https://instagram.com/inerre_interior">Instagram</a>
-                                        </ul>
-                                    </div>
+                                       <div class="col-md-8">
+                                           <h4>Main Office</h4>
+                                           <ul>
+                                               <li><a href="https://www.google.co.id/maps/place/INERRE+Interior/@-6.900453,107.603322,17z/data=!3m1!4b1!4m2!3m1!1s0x2e68e64383f4eda3:0x420267de73972b84?hl=en" target="_blank">Jl. Suka Mekar III No. 7, Bandung, Indonesia</a></li>
+                                               <li><a href="#">+62 22 2013973</a></li>
+                                               <li><a href="mailto:contact@subiantosiane.com">contact@subiantosiane.com</a></li>
+                                           </ul>
+                                       </div>
+                                       <div class="col-md-4"></div>
+                                       
+                                       
+                                   </div>
+                                   <div class="col-md-12" style="padding-top:20px;">
+                                       <div class="col-md-8">
+                                           <h4>Find Us On : </h4>
+                                           <ul>
+                                               <li>
+                                                    <a href="https://www.facebook.com/inerre.interior" target="_blank">Facebook</a>|
+                                                    <a href="https://twitter.com/inerre_interior" target="_blank">Twitter </a>|
+                                                    <a href="https://instagram.com/inerre_interior" target="_blank">Instagram</a>
+                                                </li>
+                                           </ul>
+                                       </div>
+                                       <div class="col-md-4"></div>
+                                   </div> 
                                 </div>
                             </div>
                         </section>
