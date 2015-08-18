ce115d79fc97eaca08fb3976ed667b4c26abc6ea
Willi-dev <willi.ilmukomputer@gmail.com>
2015-08-18 15:04:59 +0700

    portfolio page small / mobile screen: no book image loaded

--- application/views/public/templates/footer.php
+++ application/views/public/templates/footer.php
@@ -151,6 +151,7 @@
             // echo nothing
         }
         ?>
+        
 
         <script type="text/javascript">
             /*
@@ -160,98 +161,100 @@
 
             (function () {
                 'use strict';
+                if($(window).width() <= 1170){
+                    // nothing
+                }else{
+                    var module = {
+                        // ratio: 1.38,
+                        ratio: 3.1,
+                        init: function (id) {
+                            var me = this;
 
-                var module = {
-                    // ratio: 1.38,
-                    ratio: 3.1,
-                    init: function (id) {
-                        var me = this;
+                            // if older browser then don't run javascript
+                            if (document.addEventListener) {
+                                this.el = document.getElementById(id);
+                                this.resize();
+                                this.plugins();
+                                
+                                // on window resize, update the plugin size
+                                window.addEventListener('resize', function (e) {
+                                    var size = me.resize();
+                                    $(me.el).turn('size', size.width, size.height);
+                                    console.log(size.width);
+                                    // $(me.el).turn({
+                                    //     width: size.width, 
+                                    //     height: size.height,
+                                    //     // Elevation
+                                    //     elevation: 50,
+                                    //     // Enable gradients
+                                    //     gradients: true,
+                                    //     // Auto center this flipbook
+                                    //     autoCenter: true
+                                    // });
+                                });
+                            }
+                        },
+                        resize: function () {
+                            // reset the width and height to the css defaults
+                            this.el.style.width = '';
+                            this.el.style.height = '';
 
-                        // if older browser then don't run javascript
-                        if (document.addEventListener) {
-                            this.el = document.getElementById(id);
-                            this.resize();
-                            this.plugins();
+                            var width = this.el.clientWidth,
+                                height = Math.round(width / this.ratio),
+                                padded = Math.round(document.body.clientHeight * 0.8),
+                                ownHeight = parseInt(height * 0.96),
+                                coverPadding = parseInt((height-ownHeight)/2),
+                                ownWidth = parseInt((width/2)-coverPadding);
+                            console.log(this.el.clientWidth);
+
+                            /*console.log('clientWidth:' + this.el.clientWidth);
+                            console.log('width: ' + width);
+                            console.log('height:' + height);
+                            console.log('padded:' + padded);
+                            console.log('clientHeight:' + this.el.clientHeight);
+                            console.log(ownWidth);
+                            console.log(ownHeight);*/
+
+                            // if the height is too big for the window, constrain it
+                            if (height > padded) {
+                                height = padded;
+                                width = Math.round(height * this.ratio);
+                                // console.log('height:' + height);
+                                // console.log('width:' + width);
+                                ownHeight = parseInt(height * 0.96);
+                                coverPadding = parseInt((height-ownHeight)/2);
+                                ownWidth = parseInt((width/2)-coverPadding);
+                            }
+
+                            // set the width and height matching the aspect ratio
+                            this.el.style.width = width + 'px';
+                            this.el.style.height = height + 'px';
                             
-                            // on window resize, update the plugin size
-                            window.addEventListener('resize', function (e) {
-                                var size = me.resize();
-                                $(me.el).turn('size', size.width, size.height);
-                                console.log(size.width);
-                                // $(me.el).turn({
-                                //     width: size.width, 
-                                //     height: size.height,
-                                //     // Elevation
-                                //     elevation: 50,
-                                //     // Enable gradients
-                                //     gradients: true,
-                                //     // Auto center this flipbook
-                                //     autoCenter: true
-                                // });
+                            $('.flipbook .own-size').width(ownWidth);
+                            $('.flipbook .own-size').height(ownHeight);
+                            return {
+                                width: width,
+                                height: height,
+                                ownWidth: ownWidth,
+                                ownHeight: ownHeight
+                            };
+                        },
+                        plugins: function () {
+                            // run the plugin
+                            $(this.el).turn({
+                                // width:200,
+                                // height:80,
+                                gradients: true,
+                                acceleration: true,
+                                elevation: 50,
+                                // autoCenter: true
                             });
+                            // hide the body overflow
+                            document.body.className = 'hide-overflow';
                         }
-
-                    },
-                    resize: function () {
-                        // reset the width and height to the css defaults
-                        this.el.style.width = '';
-                        this.el.style.height = '';
-
-                        var width = this.el.clientWidth,
-                            height = Math.round(width / this.ratio),
-                            padded = Math.round(document.body.clientHeight * 0.8),
-                            ownHeight = parseInt(height * 0.96),
-                            coverPadding = parseInt((height-ownHeight)/2),
-                            ownWidth = parseInt((width/2)-coverPadding);
-                        console.log(this.el.clientWidth);
-
-                        /*console.log('clientWidth:' + this.el.clientWidth);
-                        console.log('width: ' + width);
-                        console.log('height:' + height);
-                        console.log('padded:' + padded);
-                        console.log('clientHeight:' + this.el.clientHeight);
-                        console.log(ownWidth);
-                        console.log(ownHeight);*/
-
-                        // if the height is too big for the window, constrain it
-                        if (height > padded) {
-                            height = padded;
-                            width = Math.round(height * this.ratio);
-                            // console.log('height:' + height);
-                            // console.log('width:' + width);
-                            ownHeight = parseInt(height * 0.96);
-                            coverPadding = parseInt((height-ownHeight)/2);
-                            ownWidth = parseInt((width/2)-coverPadding);
-                        }
-
-                        // set the width and height matching the aspect ratio
-                        this.el.style.width = width + 'px';
-                        this.el.style.height = height + 'px';
-                        
-                        $('.flipbook .own-size').width(ownWidth);
-                        $('.flipbook .own-size').height(ownHeight);
-                        return {
-                            width: width,
-                            height: height,
-                            ownWidth: ownWidth,
-                            ownHeight: ownHeight
-                        };
-                    },
-                    plugins: function () {
-                        // run the plugin
-                        $(this.el).turn({
-                            // width:200,
-                            // height:80,
-                            gradients: true,
-                            acceleration: true,
-                            elevation: 50,
-                            // autoCenter: true
-                        });
-                        // hide the body overflow
-                        document.body.className = 'hide-overflow';
-                    }
-                };
-                module.init('flipbook');
+                    };
+                    module.init('flipbook');
+                }
             }());
         </script>
     </body>
